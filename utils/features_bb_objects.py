bdc85100b26fbefa166c3a8ba1297eb71f90c960
benjavides <benjavides@gmail.com>
2019-06-28 13:15:10 -0400

    extract Resnet features from bb

--- utils/features_bb_objects.py
+++ utils/features_bb_objects.py
@@ -25,10 +25,10 @@ if __name__ == "__main__":
     device = torch.device(mode)
 
 
-    id_images_in_miniGQA_path = "./data/id_images_in_miniGQA.json"
-    images_miniGQA_path = "./data/img/"
-    train_graph_path = "./data/sceneGraphs/train_sceneGraphs.json"
-    val_graph_path = "./data/sceneGraphs/val_sceneGraphs.json"
+    id_images_in_miniGQA_path = "./data/miniGQA/id_images_in_miniGQA.json"
+    images_miniGQA_path = "G:/Archivos/Downloads/AI/Relation Net No Git/utils/visualizeObjects/images/"
+    train_graph_path = "./data/scene_graph/train_sceneGraphs.json"
+    val_graph_path = "./data/scene_graph/val_sceneGraphs.json"
 
     save_features_path = "./data/GQA_objects_features/"
 
@@ -51,6 +51,8 @@ if __name__ == "__main__":
 
     # --- GET valid image ids ---
     print("searching for valid ids...")
+
+    #pbar = tqdm(total=len(image_ids))
     #for image_id in image_ids[0:20]:
     for image_id in image_ids:
         if image_id in train_graph:
@@ -61,7 +63,13 @@ if __name__ == "__main__":
 
         else:
             not_seen.append(image_id)
-            continue
+        
+        #pbar.update()
+
+    #pbar.close()
+    print(f"len(image_ids) ->{len(image_ids)}")
+    print(f"len(seen_id) ->{len(seen_id)}")
+    print(f"len(seen_id)/len(image_ids) ->{len(seen_id)/len(image_ids)}")
 
     # --- GET corp bb ---
     print("getting feature of objects of each image ...")
@@ -80,22 +88,37 @@ if __name__ == "__main__":
         if len(image.shape) == 2:
             image = gray2rgb(image)
 
-        objects_img = []
+        all_objects = torch.zeros((1, 1000), device=device)
         for bb_number in scene['objects']:
             bb = scene['objects'][bb_number]
-            object_img = crop(image, ((bb['y'], image.shape[0]-(bb['y']+bb['h'])), (bb['x'], image.shape[1]-(bb['x']+bb['w'])), (0,0) ), copy=False)
-            object_resize = transform.resize(object_img, (224, 224, 3), anti_aliasing=False)
-            objects_img.append(object_resize)        
+            #print((bb['y'], bb['h']), (bb['x'], bb['w']), (image.shape[0], image.shape[1]))
+            
+            #------
+            try:
+                object_img = crop(image, ((bb['y'], image.shape[0]-(bb['y']+bb['h'])), (bb['x'], image.shape[1]-(bb['x']+bb['w'])), (0,0) ), copy=False)
+                object_resize = transform.resize(object_img, (224, 224, 3), anti_aliasing=False)
+                
+                object_tensor = torch.from_numpy(np.array(object_resize)).float().to(device)
+                object_tensor = object_tensor.permute(2,0,1)
+                object_tensor = torch.unsqueeze(object_tensor, 0)
+                #print(f"object_tensor.size() -> {object_tensor.size()}")
+
+                object_features = resnet(object_tensor)
+                #print(f"object_features.size() -> {object_features.size()}")
+
+                all_objects = torch.cat((all_objects, object_features))
+                #print(f"all_objects.size() -> {all_objects.size()}")
+            #----
+
+            except:
+                pass
         
-        objects_tensor = torch.from_numpy(np.array(objects_img)).float().to(device)
-        objects_tensor = objects_tensor.permute(0,3,1,2)
-        
-        objects_features = resnet(objects_tensor)
-        # print(f"objects_features.size() -> {objects_features.size()}")
-        # torch.Size([Num_objects, 1000])
-        
-        torch.save(objects_features, save_features_path + str(image_id) + '.pt')
-        
+        # --- Remove first zero ovject ---
+        all_objects = all_objects[1:, :]
+        #print(f"all_objects.size() -> {all_objects.size()}")
+
+        torch.save(all_objects, save_features_path + str(image_id) + '.pt')
+
         # --- Load tensor example ---
         #objects_features2 = torch.load( save_features_path + str(image_id) + '.pt')
         #print(f"objects_features2.size() -> {objects_features2.size()}")
